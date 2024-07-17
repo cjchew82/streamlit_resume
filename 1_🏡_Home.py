55034d5e6a4e59e696edcea8793428703a004021
cjchew82 <ccjuen@gmail.com>
2024-07-17 15:41:51 +0800

    upload my pdf cv

--- 1_🏡_Home.py
+++ 1_🏡_Home.py
@@ -9,7 +9,7 @@ current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 
 css_file = current_dir / "styles" / "main.css"
 
-resume_file = current_dir / "assets" / "cv_mouad.pdf"
+resume_file = current_dir / "assets" / "CJCHEW-Resume_2023_v1.pdf"
 
 profile_pic = current_dir / "assets" / "home" /"profile-pic.png"
 
