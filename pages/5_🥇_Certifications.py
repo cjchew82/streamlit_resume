fe3dc08a1ddacba88a475ea47c1491a74f2e7ee1
cjchew82 <ccjuen@gmail.com>
2024-07-17 23:34:15 +0800

    Certification 2

--- pages/5_🥇_Certifications.py
+++ pages/5_🥇_Certifications.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 from PIL import Image
+import streamlit.components.v1 as components
 
 
 # ------------ PATH SETTINGS ----------
